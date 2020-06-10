f7902eb82afc41606f61614f3c7f771a310dae2f
eltavonaranjo <ganaranjo@gmail.com>
2020-06-09 21:55:01 -0500

    personal v2. Adición de header y menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,18 @@
-/*global document, window, alert, console, require, prompt*/
+/*global jQuery */
 
-var miImage = document.querySelector('img');
-var miTitulo = document.querySelector('h1');
-var miBoton = document.querySelector('button');
-
-miImage.onclick = function () {
-    'use strict';
-    var miSrc = miImage.getAttribute('src');
-    if (miSrc === 'images/medellin.jpeg') {
-        miImage.setAttribute('src', 'images/medellin2.jpeg');
-        miTitulo.innerHTML = 'La ciudad mas bella';
-    } else {
-        miImage.setAttribute('src', 'images/medellin.jpeg');
-        miTitulo.innerHTML = 'se llama Medellín';
-    }
-};
-
-function estableceNombreUsuario() {
-    'use strict';
+jQuery('document').ready(function ($) {
     
-    var miNombre = prompt('Por favor, ingresa tu nombre.');
-    localStorage.setItem('nombre', miNombre);
-    miTitulo.textContent = 'Medellín es preciosa, ' + miNombre;
+    "use strict";
+
+    var menuBtn = $('.menu-icon'),
+        menu = $('.navigation ul');
     
-}
-
-if (!localStorage.getItem('nombre')) {
-    estableceNombreUsuario();
-} else {
-    var nombreAlmacenado = localStorage.getItem('nombre');
-    miTitulo.innerHTML = 'Bienvenido, ' + nombreAlmacenado;
-}
-
-miBoton.onclick = function () {
-    'use strict';
-    estableceNombreUsuario();
-};
+    menuBtn.click(function () {
+        
+        if (menu.hasClass('show')) {
+            menu.removeClass('show');
+        } else {
+            menu.addClass('show');
+        }
+    });
+});
